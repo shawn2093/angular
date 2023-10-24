194fa5eadb2ac0826e69d3cc5712b7cef0df7930
Shawm93 <leeboon1993@gmail.com>
2023-10-25 04:26:17 +0800

    finish section 3, pending bootstrap

---
+++ project-template/section3_projects/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://images.pexels.com/photos/1860208/pexels-photo-1860208.jpeg?cs=srgb&dl=cooked-food-1860208.jpg&fm=jpg')
+  ];
+
+  constructor() {}
+
+  ngOnInit() {
+    
+  }
+}
